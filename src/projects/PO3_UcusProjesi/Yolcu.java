10c49eccda74db4a854989645559ea3a5ac2a05d
fuatyagmur <fuatyagmurr@gmail.com>
2023-06-02 19:53:41 +0200

    2

---
+++ src/projects/PO3_UcusProjesi/Yolcu.java
@@ -0,0 +1,13 @@
+package projects.PO3_UcusProjesi;
+
+public class Yolcu {
+    String city;
+    String flytype;
+    int age;
+
+    public Yolcu(String city, String flytype, int age) {
+        this.city = city;
+        this.flytype = flytype;
+        this.age = age;
+    }
+}
